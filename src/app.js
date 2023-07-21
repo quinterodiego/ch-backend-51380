f7774f312b5057f599ba946405e71f93c6eeb738
quinterodiego <d86webs@gmail.com>
2023-07-21 00:05:33 -0300

    desafio clase27

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-import express from "express";
+import express from "express"
 import handlebars from "express-handlebars"
 import path from "path"
 import cookieParser from 'cookie-parser'
 import session from 'express-session'
 import MongoStore from 'connect-mongo'
-import passport from 'passport';
-import { Command } from "commander";
+import passport from 'passport'
+import { Command } from "commander"
 
 import { __dirname, connectMongo } from "./utils/index.js"
 import { productRouter } from "./routes/product.js"
@@ -13,8 +13,8 @@ import { productRouterView } from "./routes/product.view.js"
 import cartRouter from "./routes/carts.js"
 import { cartRouterView } from "./routes/cart.view.js"
 import { authRouter } from './routes/auth.js'
-import { iniPassport } from './config/passport.js';
-import { checkAuth, passportCall } from "./middlewares/index.js";
+import { iniPassport } from './config/passport.js'
+import { checkAuth, passportCall } from "./middlewares/index.js"
 
 const app = express();
 const PORT = 8080;
@@ -33,13 +33,17 @@ program
 
 program.parse()
 
-connectMongo();
+console.log('Options: ', program.opts())
+console.log('Valor de mode: ', program.opts().mode)
+console.log('Datos no reconocibles: ', program.args)
+
+connectMongo()
 
 // MIDDLEWARES
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "../public")));
-app.use(cookieParser());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, "../public")))
+app.use(cookieParser())
 app.use(session({
   store: MongoStore.create({ mongoUrl: 'mongodb+srv://d86webs:Diego859@cluster0.ahna6cz.mongodb.net/ecommerce', ttl: 3600 }),
   secret: 'secretCode',
@@ -53,17 +57,17 @@ app.use(passport.initialize());
 // app.use(passport.session());
 
 // HANDLEBARS
-app.engine("handlebars", handlebars.engine());
-app.set("views", path.join(__dirname, "../views"));
-app.set("view engine", "handlebars");
+app.engine("handlebars", handlebars.engine())
+app.set("views", path.join(__dirname, "../views"))
+app.set("view engine", "handlebars")
 
 // ROUTES APIS
-app.use("/api/products", productRouter);
-app.use("/api/carts", cartRouter); 
+app.use("/api/products", productRouter)
+app.use("/api/carts", cartRouter);
 
 //ROUTES VIEWS
-app.use("/products", productRouterView);
-app.use("/carts", cartRouterView);
+app.use("/products", productRouterView)
+app.use("/carts", cartRouterView)
 app.use('/auth', authRouter)
 app.get('/', (_, res) =>  res.redirect('/auth/login'))
 
@@ -122,10 +126,10 @@ app.get("*", (req, res) => {
   return res.status(404).json({
     status: "error",
     msg: "Página no encontrada"
-  });
-});
+  })
+})
 
 // LISTEN
 app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+  console.log(`Server listening on http://localhost:${PORT}`)
 });
